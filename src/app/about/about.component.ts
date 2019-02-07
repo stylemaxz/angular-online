819c7e462df7bb4fd452a2edaabe4b5bc501f561
Kamin Konggawai <kamin.k@g-able.com>
2019-02-07 14:36:57 +0700

    add file about for learn databinding ngIf ngFor ngClass

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  title = 'Hell Yah!';
+  age: number ;
+  info = { email: 'john@gmail.com' };
+  logo = './assets/images/john.png';
+  imgWidth = 100;
+  isShow = false;
+  users = ['John', 'Mary', 'Max' ];
+  course = [
+    { name: 'JAVA', price: 4000 },
+    { name: 'ANGULAR', price: 5000 },
+    { name: 'PHP', price: 2000 }
+  ];
+  myColor = 'yellow';
+  isActive = false;
+
+  constructor() {
+    this.age = 10;
+  }
 
   ngOnInit() {
+    this.title = 'About us';
+  }
+
+  go() {
+    this.title = 'เกี่ยวกับเรา';
+    this.imgWidth = 300;
+    // this.isShow = true;
+    this.isShow = !this.isShow;
+    this.myColor = 'red';
+    this.isActive = !this.isActive;
   }
 
 }
